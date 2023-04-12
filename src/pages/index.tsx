76bea70c494413af4cef9d88bff864ad84254579
blake365 <68243611+blake365@users.noreply.github.com>
2023-04-11 21:57:36 -0400

    styling updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import projects from '../data'
 
-import { IconExternalLink } from '@tabler/icons-react'
+import { motion } from 'framer-motion'
+
+import { IconExternalLink, IconBrandGithub } from '@tabler/icons-react'
 
 export default function Home() {
 	return (
@@ -17,8 +19,13 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className='max-w-4xl font-sans sm:mt-12 sm:p-6 sm:mx-auto '>
-				<div className='flex flex-col border-b border-black sm:shadow-md sm:flex-row sm:border sm:m-4 bg-blue-50'>
+			<main className='max-w-4xl mb-4 font-sans sm:mt-12 sm:p-6 sm:mx-auto '>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 0.75 }}
+					className='flex flex-col bg-blue-100 border-b border-black sm:shadow-lg sm:flex-row sm:border sm:m-4'
+				>
 					<img
 						className='object-cover transition-all border-r border-black sm:w-44 saturate-150'
 						src='/QS25_Plate1.png'
@@ -35,43 +42,104 @@ export default function Home() {
 							drilling by day and coding side projects by night.
 						</h3>
 					</div>
-				</div>
+				</motion.div>
 
-				<div className='flex flex-col max-w-3xl mx-auto mt-6 shadow-md bg-blue-50'>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 0.75 }}
+					className='flex flex-col max-w-4xl mt-12 sm:mx-auto'
+				>
+					<div className='flex flex-col p-2 border-black bg-gradient-to-r from-blue-200 via-violet-200 to-red-200 sm:border border-y sm:mx-4 last:shadow-md'>
+						<h2 className='mx-auto text-3xl font-bold'>Selected Projects</h2>
+						<div className='mt-1 w-2xl'>
+							<div className='flex justify-center mx-auto mb-1 font-semibold'>
+								Key to Symbols:
+							</div>
+							<div className='flex flex-row justify-evenly'>
+								<div className='flex flex-col'>
+									<div
+										className='w-[30px] h-[30px] border-black border mx-auto'
+										style={{ backgroundImage: 'url(/dotgrid.svg)' }}
+									></div>
+									<div>Front End</div>
+								</div>
+								<div className='flex flex-col'>
+									<div
+										className='w-[30px] h-[30px] border-black border mx-auto'
+										style={{ backgroundImage: 'url(/diags2.svg)' }}
+									></div>
+									<div>Serverless</div>
+								</div>
+								<div className='flex flex-col'>
+									<div
+										className='w-[30px] h-[30px] border-black border mx-auto'
+										style={{ backgroundImage: 'url(/bigstripe.svg)' }}
+									></div>
+									<div>Full Stack</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</motion.div>
+
+				<div className='flex flex-col max-w-4xl mt-6 sm:mx-auto'>
 					{projects.map((project) => (
-						<div className='flex flex-row border-black' key={project.title}>
+						<motion.div
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ duration: 0.5 }}
+							className='flex flex-row border-black bg-gradient-to-r from-blue-200 via-violet-200 to-red-200 sm:mx-4 last:shadow-md first:border-t'
+							key={project.title}
+						>
 							<div
-								className='w-[30px] sm:w-[50px] border border-black'
+								className='w-[35px] sm:w-[50px] border-black border-b border-l-0 sm:border-l border-x sm:border-r-0'
 								style={{ backgroundImage: `url(${project.sidebar})` }}
 							></div>
-							<div className='flex flex-col w-full px-2 pl-2 border border-black'>
-								<h2 className='text-2xl font-bold text-blue-700'>
-									{project.title}
-								</h2>
-								<div className='text-md'>{project.description}</div>
-								{project.live ? (
-									<Link
-										href={project.live}
-										className='flex flex-row justify-center font-semibold text-center align-middle bg-blue-300 border border-black w-36 hover:bg-blue-200'
-										target='_blank'
-									>
-										<IconExternalLink className='inline mx-1' />
-										<div className='mx-1'>Live Link</div>
-									</Link>
-								) : null}
-								{/* {project.github ? <Link href={project.github} /> : null} */}
+							<div className='flex flex-col w-full px-2 pt-2 pl-2 border-b border-black sm:border-x'>
+								<div>
+									<h2 className='text-3xl font-bold text-blue-900 align-baseline'>
+										{project.title}
+										{project.live ? (
+											<Link
+												href={project.live}
+												className='inline ml-2 w-36 hover:text-blue-600'
+												target='_blank'
+											>
+												<IconExternalLink className='inline' />
+											</Link>
+										) : null}
+										{project.github.length > 0
+											? project.github.map((link) => (
+													<Link
+														key={link}
+														href={link}
+														className='inline p-1 ml-2 hover:text-blue-600'
+														target='_blank'
+													>
+														<IconBrandGithub
+															className='inline'
+															width={20}
+															height={20}
+														/>
+													</Link>
+											  ))
+											: null}
+									</h2>
+									<div className='text-md'>{project.description}</div>
+								</div>
 								<div className='flex flex-wrap p-2 pl-0'>
 									{project.stack.map((item) => (
 										<span
 											key={item}
-											className='px-1 mb-1 mr-1 text-white bg-blue-700 border border-blue-900'
+											className='px-1 mb-1 mr-1 text-white bg-blue-800 border border-blue-900'
 										>
 											{item}
 										</span>
 									))}
 								</div>
 							</div>
-						</div>
+						</motion.div>
 					))}
 				</div>
 			</main>
